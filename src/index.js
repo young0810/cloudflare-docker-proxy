923624bbbdd9c8d958761a669183b6d55a56a7c8
young <993810612@qq.com>
2024-12-24 15:31:58 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,18 +6,13 @@ addEventListener("fetch", (event) => {
 const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
-  // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
-
-  // staging
-  ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
+  "docker.yangyong.xin": "https://registry-1.docker.io",
+  "quay.yangyong.xin": "https://quay.io",
+  "gcr.yangyong.xin": "https://gcr.io",
+  "k8s-gcr.yangyong.xin": "https://k8s.gcr.io",
+  "k8s.yangyong.xin": "https://registry.k8s.io",
+  "ghcr.yangyong.xin": "https://ghcr.io",
+  "cloudsmith.yangyong.xin": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
